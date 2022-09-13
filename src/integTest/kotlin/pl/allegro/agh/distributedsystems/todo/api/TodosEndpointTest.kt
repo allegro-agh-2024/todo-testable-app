d34069bd984b168b7de8db092bbb31f2abbbba1b
Adam Szadkowski <adam.szadkowski@allegro.pl>
2022-09-13 11:55:33 +0200

    Extract helper functions

--- src/integTest/kotlin/pl/allegro/agh/distributedsystems/todo/api/TodosEndpointTest.kt
+++ src/integTest/kotlin/pl/allegro/agh/distributedsystems/todo/api/TodosEndpointTest.kt
@@ -1,5 +1,10 @@
 package pl.allegro.agh.distributedsystems.todo.api
 
+import org.hamcrest.Matcher
+import org.hamcrest.Matchers.contains
+import org.hamcrest.Matchers.emptyIterable
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
@@ -15,24 +20,34 @@ class TodosEndpointTest(@Autowired private val mockMvc: MockMvc) {
 
     @Test
     fun `get empty todos`() {
-        mockMvc.get("/todos").andExpect {
-            status { is2xxSuccessful() }
-            content { json(""" { "todos": [] } """) }
+        expectTodos(emptyIterable())
+    }
+
+    @Nested
+    inner class `single saved` {
+
+        @BeforeEach
+        fun `save single todo`() {
+            saveTodo("new todo")
+        }
+
+        @Test
+        fun `get all`() {
+            expectTodos(contains("new todo"))
         }
     }
 
-    @Test
-    fun `get single`() {
+    private fun expectTodos(matcher: Matcher<Iterable<String>>) =
+        mockMvc.get("/todos").andExpect {
+            status { is2xxSuccessful() }
+            jsonPath("\$.todos", matcher)
+        }
+
+    private fun saveTodo(name: String) =
         mockMvc.post("/todos") {
             contentType = MediaType.APPLICATION_JSON
-            content = """ { "name": "new todo" } """
+            content = """ { "name": "$name" } """
         }.andExpect {
             status { is2xxSuccessful() }
         }
-
-        mockMvc.get("/todos").andExpect {
-            status { is2xxSuccessful() }
-            content { json(""" { "todos": ["new todo"] } """) }
-        }
-    }
 }
