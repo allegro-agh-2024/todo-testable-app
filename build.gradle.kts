7f050a1f8b4192dd4ad70fa677887c79fcbbe594
Adam Szadkowski <adam.szadkowski@allegro.pl>
2023-10-11 13:11:22 +0200

    Upgrade Kotlin to 1.9.10

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    kotlin("jvm") version "1.7.20"
-    kotlin("plugin.spring") version "1.7.20"
+    kotlin("jvm") version "1.9.10"
+    kotlin("plugin.spring") version "1.9.10"
 
     id("org.springframework.boot") version "2.7.5"
     id("nebula.integtest") version "9.6.3"
